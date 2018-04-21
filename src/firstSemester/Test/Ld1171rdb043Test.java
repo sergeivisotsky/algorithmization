c1056f6bde779491ed3835d03a85afc2a19d2708
Sergei <sergei.visotsky@gmail.com>
2018-04-21 20:37:05 +0300

    unit test added

---
+++ src/firstSemester/Test/Ld1171rdb043Test.java
@@ -0,0 +1,13 @@
+package firstSemester.Test;
+
+import firstSemester.Ld1171rdb043;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ld1171rdb043Test {
+    @Test
+    void mainMethodTest() {
+        Ld1171rdb043.main(new String[] {"5", "6"});
+    }
+}
