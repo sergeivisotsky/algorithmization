1cec3471a2a4c1af82b32a1f7309daf596a52082
Sergei <sergei.visotsky@gmail.com>
2018-05-02 18:55:32 +0300

    Third competition added

--- src/main/java/secondSemester/ThirdCompetition.java
+++ src/main/java/secondSemester/ThirdCompetition.java
@@ -1,78 +1,96 @@
 package secondSemester;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class ThirdCompetition {
-    private static BufferedReader reader =
-            new BufferedReader(new InputStreamReader(System.in));
-    private static void pushQuantityChecker() {
-
-    }
-
-    public static void main(String[] args) {
-        try {
-            int answer;
-            char[] one;
-            char[] two = {'a', 'b', 'c'};
-            char[] three = {'D', 'e', 'f'};
-            char[] four = {'g', 'h', 'i'};
-            char[] five = {'j', 'k', 'l'};
-            char[] six = {'M', 'n', 'o'};
-            char[] seven = {'p', 'q', 'r', 's'};
-            char[] eight = {'t', 'u', 'v'};
-            char[] nine = {'w', 'x', 'y', 'z'};
-            char[] zero = {' '};
-
-            System.out.println("1: ");
-            System.out.println("2: abc");
-            System.out.println("3: Def");
-            System.out.println("4 ghi:");
-            System.out.println("5: jkl");
-            System.out.println("6: Mno");
-            System.out.println("7: pqrs");
-            System.out.println("8: tuv");
-            System.out.println("9: wxyz");
-            System.out.println("0: space");
-            System.out.print("Answer: ");
-            answer = Integer.parseInt(reader.readLine());
-            switch (answer) {
-                case 1:
-                    break;
+    private static void symbol(String num, int count) {
+        if (numberChecker(num)) {
+            switch (Integer.parseInt(num)) {
                 case 2:
+                    String str2 = "abc";
+                    display(count, str2);
                     break;
                 case 3:
+                    String str3 = "Def";
+                    display(count, str3);
                     break;
                 case 4:
+                    String str4 = "ghi";
+                    display(count, str4);
                     break;
                 case 5:
+                    String str5 = "jkl";
+                    display(count, str5);
                     break;
                 case 6:
+                    String str6 = "Mno";
+                    display(count, str6);
                     break;
                 case 7:
+                    String str7 = "pqrs";
+                    display(count, str7);
                     break;
                 case 8:
+                    String str8 = "tuv";
+                    display(count, str8);
                     break;
                 case 9:
+                    String str9 = "wxyz";
+                    display(count, str9);
                     break;
                 case 0:
+                    if (count % 2 == 0)
+                        System.out.print(".");
+                    else
+                        System.out.print(" ");
                     break;
-                    default:
-                        System.out.println("Restricted option!");
-                        break;
+                default:
             }
-
-            System.out.print("Result: ");
-        } catch (IOException e) {
-            System.out.println("Not a number!");
         }
     }
+
+    private static void display(int count, String str) {
+        while (count > str.length()) {
+            count -= str.length();
+        }
+        System.out.print(str.charAt(count - 1));
+    }
+
+    private static boolean numberChecker(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br =
+                     new BufferedReader(new InputStreamReader(System.in))) {
+            int numCount = 0;
+            System.out.print("Enter line: ");
+            String line = br.readLine();
+
+            for (int i = 1; i < line.length(); i++) {
+                if (line.charAt(i) == line.charAt(i - 1)) {
+                    numCount++;
+                } else {
+                    symbol(String.valueOf(line.charAt(i - 1)), ++numCount);
+                    numCount = 0;
+                }
+                if (i == line.length() - 1) {
+                    symbol(String.valueOf(line.charAt(i)), ++numCount);
+                }
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Inpu-output error!");
+
+        }
+    }
+
+
 }
