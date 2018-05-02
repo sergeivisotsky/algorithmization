63848c7107ea8450a454d6f463aa22bd5eaff172
Sergei <sergei.visotsky@gmail.com>
2018-05-02 19:17:41 +0300

    backup

--- src/main/java/secondSemester/ThirdCompetition.java
+++ src/main/java/secondSemester/ThirdCompetition.java
@@ -3,95 +3,79 @@ package secondSemester;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ThirdCompetition {
-    private static void symbol(String num, int count) {
-        if (numberChecker(num)) {
-            switch (Integer.parseInt(num)) {
-                case 2:
-                    String str2 = "abc";
-                    display(count, str2);
-                    break;
-                case 3:
-                    String str3 = "Def";
-                    display(count, str3);
-                    break;
-                case 4:
-                    String str4 = "ghi";
-                    display(count, str4);
-                    break;
-                case 5:
-                    String str5 = "jkl";
-                    display(count, str5);
-                    break;
-                case 6:
-                    String str6 = "Mno";
-                    display(count, str6);
-                    break;
-                case 7:
-                    String str7 = "pqrs";
-                    display(count, str7);
-                    break;
-                case 8:
-                    String str8 = "tuv";
-                    display(count, str8);
-                    break;
-                case 9:
-                    String str9 = "wxyz";
-                    display(count, str9);
-                    break;
-                case 0:
-                    if (count % 2 == 0) {
-                        System.out.print(".");
-                    } else {
-                        System.out.print(" ");
-                    }
-                    break;
-                default:
-            }
-        }
-    }
+    private final char[] two = {'a', 'b', 'c'};
+    private final char[] three = {'D', 'e', 'f'};
+    private final char[] four = {'g', 'h', 'i'};
+    private final char[] five = {'j', 'k', 'l'};
+    private final char[] six = {'M', 'n', 'o'};
+    private final char[] seven = {'p', 'q', 'r', 's'};
+    private final char[] eight = {'t', 'u', 'v'};
+    private final char[] nine = {'w', 'x', 'y', 'z'};
+    private final char[] zero = {' '};
 
-    private static void display(int count, String str) {
-        while (count > str.length()) {
-            count -= str.length();
-        }
-        System.out.print(str.charAt(count - 1));
-    }
+    private void pushQuantityChecker(int numberIndex, char[] answer) {
+        /*char result = Arrays.toString(answer).charAt(numberIndex + 1);
+        System.out.print("Result: " + result);*/
+        for (int i = 0; i < numberIndex; i++) {
 
-    private static boolean numberChecker(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (IllegalArgumentException e) {
-            return false;
         }
     }
 
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br =
-                     new BufferedReader(new InputStreamReader(System.in))) {
-            int numCount = 0;
-            System.out.print("Enter line: ");
-            String line = br.readLine();
-
-            for (int i = 1; i < line.length(); i++) {
-                if (line.charAt(i) == line.charAt(i - 1)) {
-                    numCount++;
-                } else {
-                    symbol(String.valueOf(line.charAt(i - 1)), ++numCount);
-                    numCount = 0;
-                }
-                if (i == line.length() - 1) {
-                    symbol(String.valueOf(line.charAt(i)), ++numCount);
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(System.in));
+        ThirdCompetition competition = new ThirdCompetition();
+        String line = reader.readLine();
+        int counter = 0;
+        for (int i = 1; i < line.length(); i++) {
+            if (line.charAt(i) == line.charAt(i - 1)) {
+                counter++;
+            } else {
+                try {
+                    String temp = String.valueOf(line.charAt(i));
+                    int answer = Integer.parseInt(temp);
+                    switch (answer) {
+                        case 1:
+                            break;
+                        case 2:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 3:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 4:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 5:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 6:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 7:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 8:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 9:
+                            System.out.print(competition.two[counter]);
+                            break;
+                        case 0:
+                            if (counter % 2 == 0) {
+                                System.out.print(" ");
+                            } else {
+                                System.out.print(".");
+                            }
+                            break;
+                    }
+                } catch (IllegalArgumentException ignored) {
                 }
             }
 
-        } catch (IllegalArgumentException e) {
-            System.out.println("Input-output error!");
-
         }
     }
-
-
 }
