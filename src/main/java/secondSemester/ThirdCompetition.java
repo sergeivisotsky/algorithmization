c1d4c770239d72bc83655b1c4c0a9ec3b7dc7cd1
Sergei <sergei.visotsky@gmail.com>
2018-05-03 08:36:12 +0300

    pom.xml refactoring / third competition added

--- src/main/java/secondSemester/ThirdCompetition.java
+++ src/main/java/secondSemester/ThirdCompetition.java
@@ -5,76 +5,91 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ThirdCompetition {
-    private final char[] two = {'a', 'b', 'c'};
-    private final char[] three = {'D', 'e', 'f'};
-    private final char[] four = {'g', 'h', 'i'};
-    private final char[] five = {'j', 'k', 'l'};
-    private final char[] six = {'M', 'n', 'o'};
-    private final char[] seven = {'p', 'q', 'r', 's'};
-    private final char[] eight = {'t', 'u', 'v'};
-    private final char[] nine = {'w', 'x', 'y', 'z'};
-    private final char[] zero = {' '};
 
-    private void pushQuantityChecker(int numberIndex, char[] answer) {
-        /*char result = Arrays.toString(answer).charAt(numberIndex + 1);
-        System.out.print("Result: " + result);*/
-        for (int i = 0; i < numberIndex; i++) {
-
+    private static void character(String number, int counter) {
+        if (parseAsTheNumber(number)) {
+            switch (Integer.parseInt(number)) {
+                case 2:
+                    final String TWO = "abc";
+                    display(counter, TWO);
+                    break;
+                case 3:
+                    final String THREE = "Def";
+                    display(counter, THREE);
+                    break;
+                case 4:
+                    final String FOUR = "ghi";
+                    display(counter, FOUR);
+                    break;
+                case 5:
+                    final String FIVE = "jkl";
+                    display(counter, FIVE);
+                    break;
+                case 6:
+                    final String SIX = "Mno";
+                    display(counter, SIX);
+                    break;
+                case 7:
+                    final String SEVEN = "pqrs";
+                    display(counter, SEVEN);
+                    break;
+                case 8:
+                    final String EIGHT = "tuv";
+                    display(counter, EIGHT);
+                    break;
+                case 9:
+                    final String NINE = "wxyz";
+                    display(counter, NINE);
+                    break;
+                case 0:
+                    if (counter % 2 == 0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print(".");
+                    }
+                    break;
+            }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader =
-            new BufferedReader(new InputStreamReader(System.in));
-        ThirdCompetition competition = new ThirdCompetition();
-        String line = reader.readLine();
-        int counter = 0;
-        for (int i = 1; i < line.length(); i++) {
-            if (line.charAt(i) == line.charAt(i - 1)) {
-                counter++;
-            } else {
-                try {
-                    String temp = String.valueOf(line.charAt(i));
-                    int answer = Integer.parseInt(temp);
-                    switch (answer) {
-                        case 1:
-                            break;
-                        case 2:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 3:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 4:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 5:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 6:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 7:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 8:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 9:
-                            System.out.print(competition.two[counter]);
-                            break;
-                        case 0:
-                            if (counter % 2 == 0) {
-                                System.out.print(" ");
-                            } else {
-                                System.out.print(".");
-                            }
-                            break;
-                    }
-                } catch (IllegalArgumentException ignored) {
-                }
-            }
+    private static boolean parseAsTheNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
 
+    private static void display(int counter, String lineOfNumbers) {
+        while (counter > lineOfNumbers.length()) {
+            counter -= lineOfNumbers.length();
+        }
+
+        System.out.print(lineOfNumbers.charAt(counter - 1));
+    }
+
+    public static void main(String[] args) {
+        try(BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Input line: ");
+            String line = reader.readLine();
+            int counter = 0;
+            for (int i = 1; i < line.length(); i++) {
+                if (line.charAt(i) == line.charAt(i - 1)) {
+                    counter++;
+                } else {
+                    character(String.valueOf(line.charAt(i - 1)), ++counter);
+                    counter = 0;
+                }
+
+                if (i == line.length() - 1) {
+                    character(String.valueOf(line.charAt(i)), ++counter);
+                }
+
+            }
+        } catch (IOException e) {
+            System.out.println("Input-output error!");;
         }
     }
 }
